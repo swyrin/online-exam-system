32c2296825d53e831d3fb4b6fb41034a7519a429
NghiaTran3124 <tykul258@gmail.com>
2025-03-20 22:58:42 +0700

    feat: add frontend folder at root

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import FutureOfLearning from "./components/FutureOfLearning";
+
+function App() {
+  return (
+    <div className="App">
+      <FutureOfLearning />
+    </div>
+  );
+}
+
+export default App;

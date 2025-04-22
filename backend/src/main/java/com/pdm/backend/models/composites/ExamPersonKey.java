d2e7c8f989b11276976eb4c1f2e762243a0a9f01
Tien Dat Pham <dat20036@gmail.com>
2025-04-22 20:25:40 +0700

    fix: fix mismatching key types.

--- backend/src/main/java/com/pdm/backend/models/composites/ExamPersonKey.java
+++ backend/src/main/java/com/pdm/backend/models/composites/ExamPersonKey.java
@@ -8,8 +8,8 @@ import java.io.Serializable;
 @Embeddable
 public class ExamPersonKey implements Serializable {
     @Column(name = "person_id")
-    Long PersonID;
+    String PersonID;
 
     @Column(name = "course_id")
-    Long CourseId;
+    String CourseId;
 }
